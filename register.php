7cfaee5303b264fe86efc216bf4e7581725f5c8f
kor-2 <checkmann.corentin@gmail.com>
2021-10-07 16:56:02 +0200

    modif visu + php

--- register.php
+++ register.php
@@ -1,3 +1,45 @@
+<?php
+// http://localhost:8000
+
+require_once 'config/framework.php';
+require_once 'config/connect.php';
+
+$error=null;
+
+
+if (isset($_POST['token']) && $_POST['token'] === $_SESSION['token']) {
+  if(isset($_POST['pseudo']) && !empty($_POST['pseudo'])){
+    echo "<br>Veuillez remplir le champ";
+  }
+  if (strlen($_POST['pseudo']) < 4 ) {
+    echo '<br>Pseudo trop petit';
+  }
+  if (30< strlen($_POST['pseudo'])) {
+    echo '<br>Pseudo trop grand';
+  }
+  if(isset($_POST['email']) && !empty($_POST['email'])){
+      echo "<br>Veuillez remplir le champ";
+  }
+  if(isset($_POST['password']) && !empty($_POST['password'])){
+    echo'<br>Mot de passe incorrecte';
+  }
+  if($_POST['password'] === $_POST['passwordConf'] ){
+    echo'<br>Ce ne sont pas les même mots de passe';
+  }
+}
+
+
+
+
+
+
+
+
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="fr">
   <head>
@@ -15,130 +57,55 @@
     <title>Register</title>
   </head>
   <body>
-    
-    <section class="vh-100" style="background-color: #eee">
-      <div class="container h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-          <div class="col-lg-12 col-xl-11">
-            <div class="card text-black" style="border-radius: 25px">
-              <div class="card-body p-md-5">
-                <div class="row justify-content-center">
-                  <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
-                    Créer un compte
-                    </p>
+    <div class="m-a">
+      <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1 m-auto">
+        <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+          Créer un compte
+        </p>
 
-                    <form class="mx-1 mx-md-4">
-                      <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                          <input
-                            type="text"
-                            id="form3Example1c"
-                            class="form-control"
-                          />
-                          <label class="form-label" for="form3Example1c"
-                            >Pseudo</label
-                          >
-                        </div>
-                      </div>
-
-                      <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                          <input
-                            type="email"
-                            id="form3Example3c"
-                            class="form-control"
-                          />
-                          <label class="form-label" for="form3Example3c"
-                            >E-mail</label
-                          >
-                        </div>
-                      </div>
-
-                      <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                          <input
-                            type="password"
-                            id="form3Example4c"
-                            class="form-control"
-                          />
-                          <label class="form-label" for="form3Example4c"
-                            >Mot de passe</label
-                          >
-                        </div>
-                      </div>
-
-                      <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-key fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                          <input
-                            type="password"
-                            id="form3Example4cd"
-                            class="form-control"
-                          />
-                          <label class="form-label" for="form3Example4cd"
-                            >Confirmer votre mots de passe</label
-                          >
-                        </div>
-                      </div>
-
-                      
-
-                      <div
-                        class="d-flex justify-content-center mx-4 mb-3 mb-lg-4"
-                      >
-                        <button type="button" class="btn btn-primary btn-lg">
-                          S'inscrire
-                        </button>
-                      </div>
-                    </form>
-                  </div>
-                  <div
-                    class="
-                      col-md-10 col-lg-6 col-xl-7
-                      d-flex
-                      align-items-center
-                      order-1 order-lg-2
-                    "
-                  >
-                    <img
-                      src="https://mdbootstrap.com/img/Photos/new-templates/bootstrap-registration/draw1.png"
-                      class="img-fluid"
-                      alt="Sample image"
-                    />
-                  </div>
-                </div>
-              </div>
+        <form class="mx-1 mx-md-4" method="post" >
+          <input type="hidden"name="token" value="<?= miniToken(); ?>">
+          <div class="d-flex flex-row align-items-center mb-4">
+            <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+            <div class="form-outline flex-fill mb-0">
+              <input type="text"  class="form-control" name="pseudo" id="pseudo"/>
+              <label class="form-label" for="pseudo">Pseudo</label>
             </div>
           </div>
-        </div>
+
+          <div class="d-flex flex-row align-items-center mb-4">
+            <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+            <div class="form-outline flex-fill mb-0">
+              <input type="email"  class="form-control" name="email" id="email"/>
+              <label class="form-label" for="email">E-mail</label>
+            </div>
+          </div>
+
+          <div class="d-flex flex-row align-items-center mb-4">
+            <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+            <div class="form-outline flex-fill mb-0">
+              <input type="password"  class="form-control"  name="password" id="password"/>
+              <label class="form-label" for="password">Mot de passe</label>
+            </div>
+          </div>
+
+          <div class="d-flex flex-row align-items-center mb-4">
+            <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+            <div class="form-outline flex-fill mb-0">
+              <input type="password"  class="form-control" name="passwordConf" id="passwordConf"/>
+              <label class="form-label" for="passwordConf"
+                >Confirmer votre mot de passe</label
+              >
+            </div>
+          </div>
+
+          <div class="d-flex justify-content-center m-4">
+            <button type="submit" class="btn btn-primary ">S'inscrire</button>
+          </div>
+          
+        </form>
       </div>
-    </section>
-    <!--
-    <form class=" p-3" method="post" >
-      <div class="form-group">
-        <label for="exampleInputEmail1">Adresse E-mail</label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Mot de passe</label>
-        <input
-          type="password"
-          class="form-control"
-          id="exampleInputPassword1"
-        />
-      </div>
-      
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>-->
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
