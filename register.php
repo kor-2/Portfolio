d0a994fbe3eb7d8e1bde476191234cb7236b92d5
kor-2 <checkmann.corentin@gmail.com>
2021-10-07 14:05:55 +0200

    visu de la page

---
+++ register.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+    />
+
+    <title>Register</title>
+  </head>
+  <body>
+    
+    <section class="vh-100" style="background-color: #eee">
+      <div class="container h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-lg-12 col-xl-11">
+            <div class="card text-black" style="border-radius: 25px">
+              <div class="card-body p-md-5">
+                <div class="row justify-content-center">
+                  <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                    Créer un compte
+                    </p>
+
+                    <form class="mx-1 mx-md-4">
+                      <div class="d-flex flex-row align-items-center mb-4">
+                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                        <div class="form-outline flex-fill mb-0">
+                          <input
+                            type="text"
+                            id="form3Example1c"
+                            class="form-control"
+                          />
+                          <label class="form-label" for="form3Example1c"
+                            >Pseudo</label
+                          >
+                        </div>
+                      </div>
+
+                      <div class="d-flex flex-row align-items-center mb-4">
+                        <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                        <div class="form-outline flex-fill mb-0">
+                          <input
+                            type="email"
+                            id="form3Example3c"
+                            class="form-control"
+                          />
+                          <label class="form-label" for="form3Example3c"
+                            >E-mail</label
+                          >
+                        </div>
+                      </div>
+
+                      <div class="d-flex flex-row align-items-center mb-4">
+                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                        <div class="form-outline flex-fill mb-0">
+                          <input
+                            type="password"
+                            id="form3Example4c"
+                            class="form-control"
+                          />
+                          <label class="form-label" for="form3Example4c"
+                            >Mot de passe</label
+                          >
+                        </div>
+                      </div>
+
+                      <div class="d-flex flex-row align-items-center mb-4">
+                        <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                        <div class="form-outline flex-fill mb-0">
+                          <input
+                            type="password"
+                            id="form3Example4cd"
+                            class="form-control"
+                          />
+                          <label class="form-label" for="form3Example4cd"
+                            >Confirmer votre mots de passe</label
+                          >
+                        </div>
+                      </div>
+
+                      
+
+                      <div
+                        class="d-flex justify-content-center mx-4 mb-3 mb-lg-4"
+                      >
+                        <button type="button" class="btn btn-primary btn-lg">
+                          S'inscrire
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                  <div
+                    class="
+                      col-md-10 col-lg-6 col-xl-7
+                      d-flex
+                      align-items-center
+                      order-1 order-lg-2
+                    "
+                  >
+                    <img
+                      src="https://mdbootstrap.com/img/Photos/new-templates/bootstrap-registration/draw1.png"
+                      class="img-fluid"
+                      alt="Sample image"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--
+    <form class=" p-3" method="post" >
+      <div class="form-group">
+        <label for="exampleInputEmail1">Adresse E-mail</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Mot de passe</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+      
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>-->
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
