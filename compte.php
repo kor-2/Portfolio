23ded3af591eeec0c033b62976b44be1c5f24f26
kor-2 <checkmann.corentin@gmail.com>
2021-10-12 16:57:51 +0200

    page compte

--- compte.php
+++ compte.php
@@ -2,56 +2,206 @@
 
 require_once 'config/framework.php';
 require_once 'config/connect.php';
-require_once 'login.php';
+
+
+if (!isset($_SESSION['user'])) {
+  redirectToRoute();
+}
+
+$errors= [];
+
+
+
+
+dump($_SESSION['user']);
+  
+
+if (isset($_POST['token_pseudo']) && $_POST['token_pseudo'] === $_SESSION['token_pseudo']) {
+
+  
+
+    if (strlen($_POST['pseudo']) < 3 || strlen($_POST['pseudo']) > 30 ) 
+    {
+    $errors['pseudo'] = '<br>Pseudo trop petit ou trop grand (+ de 3 ou - de 30)';
+    }
+    else {
+      
+      $sql= "UPDATE users SET pseudo = '".$_POST['pseudo']."' WHERE id = '".$_SESSION['user']['id']."'";
+      if ($mysqli->query($sql) === true) {
+          redirectToRoute('/deconnexion.php');
+      } else {
+          echo 'Une erreur est survenue. Veuillez recommencer';
+  
+    }
+  }
+}
+
+if (isset($_POST['token_email']) && $_POST['token_email'] === $_SESSION['token_email']) {
+
+  // modif email 
+
+  if (strlen($_POST['pseudo']) < 3 || strlen($_POST['pseudo']) > 30 ) 
+  {
+  $errors['pseudo'] = '<br>Pseudo trop petit ou trop grand (+ de 3 ou - de 30)';
+  }
+  else {
+    
+    $sql= "UPDATE users SET pseudo = '".$_POST['pseudo']."' WHERE id = '".$_SESSION['user']['id']."'";
+    if ($mysqli->query($sql) === true) {
+        redirectToRoute('/deconnexion.php');
+    } else {
+        echo 'Une erreur est survenue. Veuillez recommencer';
+
+    }
+  }
+}
+
+
+  
+
+
+
+
 
 ?>
 <!doctype html>
-<html lang="en">
+<html lang="fr">
   <head>
     
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-   
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
 
     <title>Compte</title>
   </head>
   <body>
-    <h1>Compte</h1>
+    
 
-    <form action="post">
-      
-      <label for="psuedo"></label>
-      <input type="text" class="form-control" id="pseudo">
-      <button type="submit" class="btn btn-primary">Changer de pseudo</button>
-    </form>
+    <div class="m-a">
+      <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1 m-auto">
+        <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+          Compte de <?= $_SESSION['user']['pseudo'];?>
+        </p>
 
-    <form action="post">
-      <?php
+        
 
-      echo $_SESSION['pseudo'];     
-      
-      ?>
+        <form class="mx-1 mx-md-4" method="post" >
+          <input type="hidden"name="token_pseudo" value="<?= miniToken('token_pseudo'); ?>">
+          
 
-      <input type="text" class="form-control" id="email">
-      <button type="submit" class="btn btn-primary">Changer d'email</button>
-    </form>
-    <form action="post">
-      <?php
-      
-      
-      ?>
+          <div class="d-flex flex-row align-items-center mb-1">
+            <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+            <div class="form-outline flex-fill mb-0">
+              <input type="pseudo"  class="form-control" name="pseudo" id="pseudo"/>
+              <label class="form-label" for="pseudo">Pseudo</label>
+              <p><?= isset($errors['pseudo']) ? $errors['pseudo'] : ''; ?></p>
 
-      <input type="text" class="form-control" id="password">
-      <button type="submit" class="btn btn-primary">Changer de mot de passe</button>
-    </form>
+              
+            </div>
+          </div>
+          
+          <button type="submit" id="btnpseudo"  class="btn btn-primary ">Changer de pseudo</button>
+          
+          
+        </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <form class="mx-1 mx-md-4" method="post" >
+          <input type="hidden"name="token" value="<?= miniToken(); ?>">
+          
+
+          <div class="d-flex flex-row align-items-center mb-4">
+            <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+            <div class="form-outline flex-fill mb-0">
+              <input type="email"  class="form-control" name="email" id="email"/>
+              <label class="form-label" for="email">E-mail</label>
+              
+            </div>
+          </div>
+
+          
+
+          <div class="d-flex justify-content-center m-4">
+            <button type="submit" id="submit" name="submit" class="btn btn-primary ">Changer d'email</button>
+          </div>
+          
+        </form>
+        
+
+
+
+
+
+
+
+
+
+        <form class="mx-1 mx-md-4" method="post" >
+          <input type="hidden"name="token" value="<?= miniToken(); ?>">
+          
+
+          <div class="d-flex flex-row align-items-center mb-4">
+            <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+            <div class="form-outline flex-fill mb-0">
+              <input type="password"  class="form-control" name="password" id="password"/>
+              <label class="form-label" for="password">Mot de passe</label>
+              
+            </div>
+          </div>
+
+          <div class="d-flex justify-content-center m-4">
+            <button type="submit" id="submit" name="submit" class="btn btn-primary ">Changer de mot de passe</button>
+          </div>
+          
+        </form>
+
+        <div class="d-flex justify-content-center m-4">
+            <button type="submit" id="submit" name="submit" class="btn btn-primary ">Supprimer la compte</button>
+        </div>
+
+
 
 
 
     
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
 
     
   </body>
